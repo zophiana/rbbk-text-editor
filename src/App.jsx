56b4ab8872f2960fcf866d9cfe4e237308e55ff5
zophiana <53372671+zophiana@users.noreply.github.com>
2025-09-24 08:17:42 +0200

    add file dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import "./App.css";
+import { open } from "@tauri-apps/plugin-dialog";
+import { useState } from "react";
 
 function App() {
+  const [fileName, setFileName] = useState("Some text");
   return (
     <main className="w-screen h-screen">
+      <button
+        onClick={async () => {
+          const file = await open({
+            multiple: false,
+            directory: false,
+            filters: [
+              {
+                name: "Text Files",
+                extensions: ["txt", "md"],
+              },
+            ],
+          });
+          setFileName(file);
+        }}
+      >
+        Open file
+      </button>
       <textarea
         name="text-editor"
         id="text-editor"
         className="size-full resize-none border-none font-mono focus:outline-none"
-      >
-        Some example text
-      </textarea>
+        value={fileName}
+        onChange={(event) => setFileName(event.target.value)}
+      ></textarea>
     </main>
   );
 }
